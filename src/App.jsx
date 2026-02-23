ad8fd77d1eb289badc0c408d275ec1ef8365beb8
Swapnadip Paul <159642690+SwapnadipPaul@users.noreply.github.com>
2026-02-23 15:28:48 +0530

    changes 4b: Vercel Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@
 
 import { Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
